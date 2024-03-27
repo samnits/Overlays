b2dced423fe8f19a0696b221b9735d12a713a01c
samnits <sakshammaurya18@gmail.com>
2024-03-27 18:57:14 +0530

    add side js

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-const imgChange=()=>{
+// const imgChange=()=>{
     
-}
-document.getElementById('img1').addEventListener("onclick",imgChange)
+// }
+// document.getElementById('img1').addEventListener("onclick",imgChange)
+
+console.log("helllo");
+let x=document.querySelector(".nav3layer")
+x.addEventListener('click',()=>{
+    let y=document.querySelector(".sidebar");
+    y.classList.toggle("sidelist");
+})
+console.log("helllo");
